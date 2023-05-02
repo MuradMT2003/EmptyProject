cfdc4bde99333b5f2cb3f6cb26e4095219a39df7
muradfm <muradfm@CAVR4>
2023-05-02 12:22:00 +0400

    write extension method

--- EmptyProject/Program.cs
+++ EmptyProject/Program.cs
@@ -1,13 +1,18 @@
-﻿using EmptyProject.Models;
+﻿
+//using EmptyProject.Models;
 
-MyString ms = new MyString();
-string some = "jjjj";
-var netice= ms.Split(some, 'j');
-foreach (var item in netice)
-{
-    Console.WriteLine(item);
-}
-Console.WriteLine(netice.Length);
-
+//MyString ms = new MyString();
+//string some = "jjjj";
+//var netice= ms.Split(some, 'j');
+//foreach (var item in netice)
+//{
+//    Console.WriteLine(item);
+//}
+//Console.WriteLine(netice.Length);
+#region Task
+//Task-her bir classin unique bir id-si olsun ve bu id non-static property olsun
+//operator overriding(==)
+//object overriding
+#endregion
 
 
